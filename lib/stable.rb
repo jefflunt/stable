047ea32fd67e3c9bd8dc1f387453900bdfad3278
jefflunt <jefflunt@gmail.com>
2025-09-14 17:17:57 -0500

    works for me

--- lib/stable.rb
+++ lib/stable.rb
@@ -1,30 +1,5 @@
-# stable is a library for recording and replaying method calls. the idea of
-# this is to reduce the amount of manual unit tests you need to write, while
-# keeping the stability/notification system that unit tests provide.
-#
-# usage:
-#
-#   require 'stable'
-#
-#   # stable uses an IO-like object to write the captured inputs/outputs.
-#   # if you don't set this, the default it to print the interaction records to
-#   # $stdout, so you could also pipe the result to another place.
-#   Stable.storage = File.open('captured_calls.jsonl', 'a')
-#
-#   # wrap a method on a given class
-#   Stable.capture(MyClass, :my_method)
-#
-#   # enable runtime input/output capture
-#   Stable.enable!
-#
-#   MyClass.my_metehod  # this will be captures by stable
-#
-#   # disable input/output capture
-#   Stable.disable!
-#
-#   # replay captured calls, which gives you a unit test-list pass/fail
-#   record = JSON.parse(File.read('captured_calls.jsonl').lines.first)
-#   Stable.replay(record)
+# `stable` is a library for recording and replaying method calls.
+# See README.md for detailed usage instructions.
 require_relative 'stable/spec'
 
 module Stable
@@ -121,4 +96,3 @@ module Stable
     end
   end
 end
-
